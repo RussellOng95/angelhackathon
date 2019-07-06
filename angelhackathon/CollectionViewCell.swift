07de4611019eb015279743caf3958241effe5bc8
russell <russell>
2019-07-06 16:11:16 +0800

    finished buttons

---
+++ angelhackathon/CollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.swift
+//  angelhackathon
+//
+//  Created by Russell Ong on 6/7/19.
+//  Copyright © 2019 Russell Ong. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+}
