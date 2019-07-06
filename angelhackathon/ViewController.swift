436d55888590f49e358fe866dacd939a99c667d7
russell <russell>
2019-07-06 15:07:43 +0800

    Initial Commit

---
+++ angelhackathon/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  angelhackathon
+//
+//  Created by Russell Ong on 6/7/19.
+//  Copyright © 2019 Russell Ong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
